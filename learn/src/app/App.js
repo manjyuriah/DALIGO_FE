3c1673a3ead72b239bb47a3e150ca9c83803ceba
kateHwang <ghkd6385@gmail.com>
2022-06-25 00:23:27 +0900

    commit

--- learn/src/app/App.js
+++ learn/src/app/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import '../assets/fonts/font.css';
 
 function App() {
   return (
     <div className="App">
       <h1> Learn Team 프로젝트 생성중입니다.</h1>
+      <h2>font 수정중입니다.</h2>
     </div>
   );
 }
