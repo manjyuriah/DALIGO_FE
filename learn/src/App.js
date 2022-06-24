b3bbc5dda60849b7c763397ddfb13fac94de7b85
kateHwang <ghkd6385@gmail.com>
2022-06-25 00:44:03 +0900

    font 브라우저 확인

--- learn/src/App.js
+++ learn/src/App.js
@@ -1,12 +1,12 @@
 
 import './App.css';
-import '../assets/fonts/font.css';
 
 function App() {
   return (
     <div className="App">
       <h1> Learn Team 프로젝트 생성중입니다.</h1>
       <h2>font 수정중입니다.</h2>
+      <h3>font 수정중</h3>
     </div>
   );
 }
