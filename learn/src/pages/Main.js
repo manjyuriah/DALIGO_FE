cb41d6d66933f3307dd110245c6e6df4b96c45bb
minkim <manjyuriah01@gmail.com>
2022-06-29 12:29:26 +0900

    STYLE 피그마 수정된 디자인 참고

--- learn/src/pages/Main.js
+++ learn/src/pages/Main.js
@@ -4,46 +4,64 @@ const Main = () => {
   return (
     <>
       <section className="mainTop">
-        <header className="mainHeader">
-          <h1>LOGO</h1>
-          <div className="notice">notice</div>
-        </header>
-        <nav className="mainNav">
-          <div className="nickName">야근요정</div>
-          <div className="mainBtns">
-            <button>시험 치러 가기</button>
-            <button>학원 찾으러 가기</button>
-          </div>
-        </nav>
+        <div className="mainInner">
+          <header className="mainHeader">
+            <h1>LOGO</h1>
+            <div className="notice">notice</div>
+          </header>
+          <nav className="mainNav">
+            <div className="nickName">야근요정</div>
+            <div className="mainBtns">
+              <button>시험 치기</button>
+              <button>학원 찾기</button>
+            </div>
+          </nav>
+        </div>
       </section>
       <section className="mainBody">
-        <div className="testBox">
-          <div>시험정보 / 공지사항</div>
-          <div>시험문제 안내</div>
-          <div>기능 및 도로주행 시험 대비</div>
+        <div className="mainInner">
+          <div className="testBox">
+            <div></div>
+            <div></div>
+          </div>
+          <div className="prepareTest">기능 및 도로주행 시험 대비</div>
         </div>
       </section>
       <section className="mainCommunity">
-        <p className="driveManner">초보자가 꼭 기억해야 할 운전 매너란?</p>
-        <div className="communityBox">
-          <h3>커뮤니티</h3>
-          <div className="communityContent">
-            <div className="contentTop">
-              <div>이런저런일</div>
-              <button>더보기 &gt;</button>
+        <div className="mainInner">
+          <p className="driveManner">초보자가 꼭 기억해야 할 운전 매너란?</p>
+          <div className="communityBox">
+            <h2>커뮤니티</h2>
+            <div className="communityContent">
+              <div className="contentTop">
+                <div>이런저런일</div>
+                <button>더보기 &gt;</button>
+              </div>
+              <ul className="driverNews">
+                <li>
+                  <div class="newsImg" alt="newsImg"></div>
+                  <div className="newsTitle">
+                    이 정도면 최고 난이도 시험 아니냐?!{" "}
+                  </div>
+                </li>
+                <li></li>
+                <li></li>
+              </ul>
+              <ul className="bannerDisc">
+                <li></li>
+                <li></li>
+                <li></li>
+              </ul>
             </div>
-            <ul className="bannerDisc">
-              <li></li>
-              <li></li>
-              <li></li>
-            </ul>
           </div>
         </div>
-        <div className="pickBox">
+      </section>
+      <section className="pickBox">
+        <div className="mainInner">
           <div className="siteInform">
             <div>#Pick</div>
             <p>우리동네 운전 연습하기 좋은 곳 없을까?</p>
-            <span>#서울근교 #울산 #부산 #광주</span>
+            <p>#서울근교 #울산 #부산 #광주</p>
           </div>
           <div className="siteImage">
             <img alt="운전연습 장소"></img>
@@ -51,12 +69,14 @@ const Main = () => {
         </div>
       </section>
       <footer>
-        <div className="footerBox">
-          <button>소식</button>
-          <button>시험</button>
-          <button>홈</button>
-          <button>위치안내</button>
-          <button>내정보</button>
+        <div className="mainInner">
+          <div className="footerBox">
+            <button>소식</button>
+            <button>시험</button>
+            <button>홈</button>
+            <button>위치안내</button>
+            <button>내정보</button>
+          </div>
         </div>
       </footer>
     </>
