30617d8881039ce287cca92b5bc2699c5a1d3d76
kateHwang <ghkd6385@gmail.com>
2022-07-02 12:01:29 +0900

    fix-navigation-component

---
+++ learn/src/components/MainNavigation.js
@@ -0,0 +1,18 @@
+//하단 고정 navigation bar 내용입니다.
+
+const MainNavigation = () => {
+    return(
+        <>
+            <nav className="fix-navigation">
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </nav>
+        </>
+    )
+}
+export default MainNavigation;
