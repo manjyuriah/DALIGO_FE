d08f63f490ed6c4706b2e41e318ff34ac2ad83cd
kateHwang <ghkd6385@gmail.com>
2022-07-16 22:30:07 +0900

    commit-완료컴포넌트

---
+++ learn/src/components/SuccessLogin.js
@@ -0,0 +1,17 @@
+//가입완료 창입니다.
+
+const SuccessLogin = () => {
+    return(
+        <>
+            <div className="successLogin-wrap">
+                <div className="successLogin-image">
+                    <img src="" alt="성공했을경우의 이미지" /> 
+                </div>
+                <h3>뚝딱! 가입완료되었습니다.</h3>
+                <div>
+                    {/*완료button-컴포넌트 들어갈 예정입니다.*/}
+                </div>
+            </div>
+        </>
+    )
+}
